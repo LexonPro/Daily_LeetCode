a58ea80dc3ae943223f786a7d5f1999d65ff5bb4
Shikhar Maurya <111562809+LexonPro@users.noreply.github.com>
2026-04-05 14:08:37 +0530

    Time: 3 ms (46.4%), Space: 10.5 MB (13.45%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        vector<int> m(4, 0); // initialize size 4
+
+        for(int i = 0; i < moves.size(); i++){
+            if(moves[i] == 'U'){
+                m[0]++;
+            }
+            else if(moves[i] == 'D'){
+                m[1]++;
+            }
+            else if(moves[i] == 'L'){
+                m[2]++;
+            }
+            else if(moves[i] == 'R'){
+                m[3]++;
+            }
+        }
+
+        if(m[0] == m[1] && m[2] == m[3]) return true;
+
+        return false;
+    }
+};
