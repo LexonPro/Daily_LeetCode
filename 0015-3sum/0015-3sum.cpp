30bf2fa2b602cff82391988bacb75d49bf90b147
Shikhar Maurya <111562809+LexonPro@users.noreply.github.com>
2026-09-02 16:00:16 +0530

    Time: 47 ms (63.1%), Space: 29 MB (73.19%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
 
-        int n = nums.size();
+        sort(nums.begin(), nums.end());
 
-        for(int i = 0; i < n - 2; i++) {
+        for (int i = 0; i < nums.size() - 2; i++) {
 
            
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
 
             int j = i + 1;
-            int k = n - 1;
+            int k = nums.size() - 1;
 
-            while(j < k) {
+            while (j < k) {
+
                 int sum = nums[i] + nums[j] + nums[k];
 
-                if(sum == 0) {
+                if (sum == 0) {
                     ans.push_back({nums[i], nums[j], nums[k]});
 
+                    while (j < k && nums[j] == nums[j + 1])
+                        j++;
 
-                    while(j < k && nums[j] == nums[j + 1]) j++;
-                    while(j < k && nums[k] == nums[k - 1]) k--;
+                    while (j < k && nums[k] == nums[k - 1])
+                        k--;
 
                     j++;
                     k--;
                 }
-                else if(sum < 0) {
+                else if (sum < 0) {
                     j++;
                 }
                 else {
